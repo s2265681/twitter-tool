7a96c830f92be7eaf6f012c4cae7249cae77b7f6
佳伟 <s2265681@163.com>
2024-05-07 11:46:54 +0800

    fix: 完成搜索

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -6,14 +6,17 @@ import "./index.scss";
 export default ({
   placeholder,
   onChange,
+  inputRef,
 }: {
   placeholder: string;
   onChange;
+  inputRef;
 }) => {
   return (
     <div className="tt_input_wrapper flex items-center px-3 gap-[6px] h-[34px] rounded-lg w-full">
       <SearchSvg className="w-[18px] h-[18px] search_svg"></SearchSvg>
       <input
+        ref={inputRef}
         className="tt_input w-full"
         placeholder={placeholder}
         onKeyDown={(e) => {
--- feature/PersonalEnhance/components/Filters.tsx
+++ feature/PersonalEnhance/components/Filters.tsx
@@ -1,9 +1,12 @@
 import Input from "~components/Input";
 import Dropdown from "../../../components/Dropdown";
 import { created_at, followers_count, following_count } from "../config";
-import React from "react";
+import React, { useRef, useState } from "react";
+import CloseSvg from "react:./close.svg";
 
 export default ({ filters, setParams }) => {
+  const inputRef = useRef(null);
+  const [users, setUsers] = useState<string[]>([]);
   return (
     <div className="px-4">
       <div className="flex items-center gap-4 justify-start mt-5">
@@ -44,17 +47,59 @@ export default ({ filters, setParams }) => {
           label="Joined"
         ></Dropdown>
       </div>
-      <div className="mt-[10px] mb-[10px] w-full">
+      <div className="mt-[10px] mb-[10px] w-full gap-6 flex items-center">
         <Input
-          placeholder="search anything"
+          inputRef={inputRef}
+          placeholder="input no more than 10@"
           onChange={(value) => {
-            setParams((_params) => ({
-              ..._params,
-              text: value,
-            }));
+            // setParams((_params) => ({
+            //   ..._params,
+            //   text: value,
+            // }));
+            if (users.length < 10) {
+              setUsers([...users, value]);
+            }
+            inputRef.current.value = "";
           }}
         ></Input>
+        <div
+          className="text-white bg-[#1E9CF1] w-[76px] h-[34px] flex justify-center items-center rounded cursor-pointer"
+          onClick={() => {
+            if (users.length < 10 && inputRef.current.value) {
+              console.log(inputRef.current.value, "curretn,,,,11,");
+              setUsers([...users, inputRef.current.value]);
+            }
+            inputRef.current.value = "";
+            console.log(users, "users......");
+          }}
+        >
+          search
+        </div>
       </div>
+
+      {/* 搜索词的模块 */}
+      {users.length > 0 && (
+        <div className="flex w-full flex-wrap gap-[10px] my-4">
+          {users.map((el) => {
+            return (
+              <div
+                key={el}
+                className="w-fit gap-3 px-3 py-3 rounded-xl flex justify-between bg-[rgba(255,197,139,0.2)] items-center"
+              >
+                <div className="text-[#F17F0B] text-[12px]">{el}</div>
+                <CloseSvg
+                  className="flex-none cursor-pointer"
+                  onClick={() => {
+                    const newUser = users.filter((item) => item !== el);
+                    console.log(newUser, "newUser...");
+                    setUsers(newUser);
+                  }}
+                ></CloseSvg>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
