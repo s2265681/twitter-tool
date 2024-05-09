07456d6cd3fa18218089990cd8a6ff540b344668
佳伟 <s2265681@163.com>
2024-05-09 21:08:02 +0800

    feat: 增加tooltip

--- feature/PersonalEnhance/components/CommonList.tsx
+++ feature/PersonalEnhance/components/CommonList.tsx
@@ -2,11 +2,45 @@ import React from "react";
 import MyLoadingSvg from "react:./loading.svg";
 import Filters from "./Filters";
 import List from "./List";
+import Tooltip from "./Tooltip";
 
 export default ({ filters, dataSource, setParams, loading, setPageNo }) => {
   console.log(loading, "loading...");
+
+  const avatarArr = [
+    {
+      title: "@Rockshang",
+      src: "https://d149xzut2sq6e3.cloudfront.net/upload/d10b52d9.png",
+    },
+    {
+      title: "@A18n",
+      src: "https://d149xzut2sq6e3.cloudfront.net/upload/6fb8b426.png",
+    },
+    {
+      title: "@dhDUDDUh_djfudhiuafhvui",
+      src: "https://d149xzut2sq6e3.cloudfront.net/upload/76d207b8.png",
+    },
+  ];
   return (
     <div className="" id="xxx">
+      <div className="p-4 m-3 flex items-center relative left-[100px]">
+        {avatarArr.map((item, index) => {
+          return (
+            <div className="" key={item.title}>
+              <Tooltip title={item.title}>
+                <img
+                  className={`avatar relative z-[${avatarArr.length - index}]`}
+                  src={item.src}
+                  style={{
+                    left: -index * 8 + "px",
+                  }}
+                ></img>
+              </Tooltip>
+            </div>
+          );
+        })}
+      </div>
+
       <Filters
         filters={filters}
         setParams={setParams}
---
+++ feature/PersonalEnhance/components/Tooltip/index.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useRef, useState } from "react";
+import "./style.scss";
+import React from "react";
+import ArrowSvg from "react:./arrow.svg";
+
+export default ({ children, title }) => {
+  const [open, setOpen] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const targetEle = useRef(null);
+
+  useEffect(() => {
+    if (targetEle.current) {
+      const { x, y, width, height } =
+        targetEle.current.childNodes[0].getBoundingClientRect();
+      const targetLeftPoint = x + width / 2;
+      const targetTopPoint = y + height + 8;
+      setPosition({
+        x: targetLeftPoint,
+        y: targetTopPoint,
+      });
+    }
+  }, [targetEle.current]);
+
+  return (
+    <div
+      className=" relative  cursor-pointer "
+      onMouseEnter={() => {
+        setOpen(true);
+      }}
+      onMouseLeave={() => {
+        setOpen(false);
+      }}
+    >
+      <div className="w-fit" ref={targetEle}>
+        {children}
+      </div>
+      {open && (
+        <div
+          className="tip fixed w-fit px-3 py-1 h-[30px] bg-[#1D2129] rounded-sm "
+          style={{
+            top: position.y + "px",
+            left: position.x + "px",
+            transform: "translateX(-50%)",
+          }}
+        >
+          <div className="text-white text-[14px]">{title}</div>
+          <ArrowSvg
+            className=" absolute left-1/2 top-0"
+            style={{
+              transform: "translate(-50%, -4px)",
+            }}
+          ></ArrowSvg>
+        </div>
+      )}
+    </div>
+  );
+};
